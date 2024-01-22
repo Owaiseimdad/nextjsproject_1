ab171f0382eb51b97f34098748e39235696638a5
Owaise Imdad <owaiseimdad@gmail.com>
2024-01-22 20:14:07 +0530

    Adding all the files and pngs necessary

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>@2024 - Owaise Imdad - All right reserved in RBI 😅</div>
+    </div>
+  );
+};
+
+export default Footer;
